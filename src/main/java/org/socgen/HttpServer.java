8e22c958ef621a841019ccd924c4a7d7f0effcf3
Lohit <lohitmkudlannavar42@gmail.com>
2025-06-30 22:47:31 +0530

    Static page response

--- src/main/java/org/socgen/HttpServer.java
+++ src/main/java/org/socgen/HttpServer.java
@@ -4,6 +4,12 @@ import org.socgen.config.Configuration;
 import org.socgen.config.ConfigurationManager;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 @SpringBootApplication
 public class HttpServer
 {
@@ -14,6 +20,32 @@ public class HttpServer
 
         System.out.println("Server started at port : " + conf.getPort());
         System.out.println("Server started at webRoot : " + conf.getWebroot());
+
+        try {
+            ServerSocket serverSocket = new ServerSocket(conf.getPort());
+            Socket socket = serverSocket.accept();
+
+            InputStream inputStream = socket.getInputStream();
+            OutputStream outputStream = socket.getOutputStream();
+
+            String html = "<html><head><title>NTNMK</title></head><body><h2>Hey this webpage is served by my http socket code</h2></body></html>";
+            final String CRLF = "\n\r";
+
+            String response = "HTTP/1.1 200 OK" + CRLF +
+                    "Content-Length: " + html.getBytes().length + CRLF +
+                    CRLF +
+                    html +
+                    CRLF + CRLF;
+
+            outputStream.write(response.getBytes());
+
+            serverSocket.close();
+            socket.close();
+            inputStream.close();
+            outputStream.close();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
 
     }
 }
